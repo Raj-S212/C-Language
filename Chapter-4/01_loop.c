8ac65e333fe14066847c14614467186fc458e6ce
Raj-S212 <rajccc70@gmail.com>
2026-03-25 23:57:05 +0530

    Fixed Chapter-4 files tracking

---
+++ Chapter-4/01_loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    printf("Happy new year\n");
+    printf("Happy new year\n");
+    printf("Happy new year\n");
+    printf("Happy new year\n");
+    printf("Happy new year\n");
+    return 0;
+}
---
+++ Chapter-4/06_Increament_dec.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int i = 5;
+     printf("The value of i is %d\n", i);
+    i= i+5;
+    printf("The value of i is %d\n", i);
+    i++;
+    printf("The value of i is %d\n", i);
+    i--;
+    printf("The value of i is %d\n", ++i);
+
+    i+=2; //Same as i =i +2
+    printf("The value of i is %d\n", i);
+
+    return 0;
+}
